5feeceb006840d3112b1e3cead738fd0bc8eeff9
Eren Dhoheiri <eren.dhoheiri@efishery.com>
2022-12-11 21:56:31 +0700

    first commit

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ const generetPdf = async (name, pimkot) => {
     return res.arrayBuffer();
   });
 
-  //   const exFont = await fetch('./Ubuntu-Regular.ttf').then(res => {
-  //     return res.arrayBuffer();
-  //   });
+  const exFont = await fetch('./Righteous-Regular.ttf').then(res => {
+    return res.arrayBuffer();
+  });
 
   const pdfDoc = await PDFDocument.load(exBytes);
 
   pdfDoc.registerFontkit(fontkit);
-  //   const myFont = await pdfDoc.embedFont(exFont);
-  const timesRomanFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+  const myFont = await pdfDoc.embedFont(exFont);
+  //   const timesRomanFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
 
-  let nameWidth = await timesRomanFont.widthOfTextAtSize(name, 18);
-  let pimkotWidth = await timesRomanFont.widthOfTextAtSize(pimkot, 12);
+  let nameWidth = await myFont.widthOfTextAtSize(name, 18);
+  let pimkotWidth = await myFont``.widthOfTextAtSize(pimkot, 12);
 
   const pages = pdfDoc.getPages();
   const firstP = pages[0];
@@ -40,15 +40,15 @@ const generetPdf = async (name, pimkot) => {
     x: (width - nameWidth) / 2,
     y: 85,
     size: 18,
-    font: timesRomanFont,
+    font: myFont,
     color: rgb(0, 0, 0)
   });
 
   firstP.drawText(pimkot, {
     x: (width - pimkotWidth) / 2,
-    y: 65,
+    y: 70,
     size: 12,
-    font: timesRomanFont,
+    font: myFont,
     color: rgb(0, 0, 0)
   });
 
