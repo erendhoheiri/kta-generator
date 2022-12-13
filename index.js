17a7df42496dbc46a6bc5bd991cb3a1ededb4e22
Eren Dhoheiri <eren.dhoheiri@efishery.com>
2022-12-13 14:28:17 +0700

    first commit

--- index.js
+++ index.js
@@ -25,12 +25,13 @@ submit.addEventListener('click', async event => {
 
   if (
     name.value === '' ||
-    pimkot.value === '' ||
-    arrival.value === '' ||
-    phone.value === '' ||
-    keberangkatan.value === '' ||
-    kepulangan.value === '' ||
-    position.value === ''
+    pimkot.value === ''
+    // ||
+    // arrival.value === '' ||
+    // phone.value === '' ||
+    // keberangkatan.value === '' ||
+    // kepulangan.value === '' ||
+    // position.value === ''
   ) {
     setTimeout(() => {
       loading.classList.add('d-none');
@@ -41,38 +42,38 @@ submit.addEventListener('click', async event => {
       alertError.classList.add('d-none');
     }, 10000);
   } else {
-    await store
-      .append('Sheet1', [
-        {
-          nama: name.value,
-          nomer_hp: phone.value,
-          asal_pimkot: pimkot.value,
-          regional_keberangkatan: arrival.value,
-          tanggal_berangkat: keberangkatan.value,
-          tanggal_pulang: kepulangan.value,
-          posisi_saat_ini: position.value,
-          deskripsi_rekomendasi: recomendation.value
-        }
-      ])
-      .then(res => {
-        if (res) {
-          alertSuccess.classList.remove('d-none');
-          setTimeout(() => {
-            alertSuccess.classList.add('d-none');
-          }, 10000);
-          textLoading.textContent = '...';
-        } else {
-          alertError.classList.remove('d-none');
-          setTimeout(() => {
-            alertError.classList.add('d-none');
-          }, 10000);
-        }
-      })
-      .catch(error => {
-        alert(
-          'There has been a problem with your fetch operation: ' + error.message
-        );
-      });
+    // await store
+    //   .append('Sheet1', [
+    //     {
+    //       nama: name.value,
+    //       nomer_hp: phone.value,
+    //       asal_pimkot: pimkot.value,
+    //       regional_keberangkatan: arrival.value,
+    //       tanggal_berangkat: keberangkatan.value,
+    //       tanggal_pulang: kepulangan.value,
+    //       posisi_saat_ini: position.value,
+    //       deskripsi_rekomendasi: recomendation.value
+    //     }
+    //   ])
+    //   .then(res => {
+    //     if (res) {
+    //       alertSuccess.classList.remove('d-none');
+    //       setTimeout(() => {
+    //         alertSuccess.classList.add('d-none');
+    //       }, 10000);
+    //       textLoading.textContent = '...';
+    //     } else {
+    //       alertError.classList.remove('d-none');
+    //       setTimeout(() => {
+    //         alertError.classList.add('d-none');
+    //       }, 10000);
+    //     }
+    //   })
+    //   .catch(error => {
+    //     alert(
+    //       'There has been a problem with your fetch operation: ' + error.message
+    //     );
+    //   });
 
     textLoading.textContent = 'Generate Kartu Anggota';
 
@@ -113,7 +114,7 @@ encodeImageFileAsURL = element => {
 const generetPdf = async (name, pimkot) => {
   const { PDFDocument, StandardFonts, rgb } = PDFLib;
 
-  const exBytes = await fetch('./Cert.pdf').then(res => {
+  const exBytes = await fetch('./Cer.pdf').then(res => {
     return res.arrayBuffer();
   });
 
@@ -127,26 +128,26 @@ const generetPdf = async (name, pimkot) => {
   const myFont = await pdfDoc.embedFont(exFont);
   //   const timesRomanFont = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
 
-  let nameWidth = await myFont.widthOfTextAtSize(name, 18);
-  let pimkotWidth = await myFont.widthOfTextAtSize(pimkot, 12);
+  let nameWidth = await myFont.widthOfTextAtSize(name, 20);
+  let pimkotWidth = await myFont.widthOfTextAtSize(pimkot, 16);
 
   const pages = pdfDoc.getPages();
   const firstP = pages[0];
-  const { width } = firstP.getSize();
+  const { width, height } = firstP.getSize();
   firstP.drawText(name, {
     x: (width - nameWidth) / 2,
-    y: 85,
-    size: 18,
+    y: 94,
+    size: 20,
     font: myFont,
-    color: rgb(0, 0, 0)
+    color: rgb(1, 1, 1)
   });
 
   firstP.drawText(pimkot, {
     x: (width - pimkotWidth) / 2,
-    y: 70,
-    size: 12,
+    y: 67,
+    size: 16,
     font: myFont,
-    color: rgb(0, 0, 0)
+    color: rgb(1, 1, 1)
   });
 
   async function image() {
@@ -157,10 +158,10 @@ const generetPdf = async (name, pimkot) => {
     const jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
 
     firstP.drawImage(jpgImage, {
-      x: 46,
-      y: 117,
-      width: 134,
-      height: 134
+      x: 66,
+      y: 134,
+      width: 137,
+      height: 164
     });
   }
   await image();
